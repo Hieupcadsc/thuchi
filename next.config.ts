99f8ed39799402a9c44ba1beeab8fa0527db9772
Hiếu Ngô Minh <hieu.m.ngokc@gmail.com>
2025-05-29 14:53:23 +0000

    Dễ nhất để có "app" trên điện thoại: Biến ứng dụng web hiện tại thành PWA. làm cách này ojk không

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,19 @@
+
 import type {NextConfig} from 'next';
+import withPWAInit from 'next-pwa';
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: isDev,
+  // buildExcludes: [/middleware-manifest.json$/], // Example: exclude files from caching by service worker
+  // fallbacks: { // Example: fallback for offline pages
+  //   document: '/offline', // will be served when user is offline
+  // },
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -20,4 +35,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
